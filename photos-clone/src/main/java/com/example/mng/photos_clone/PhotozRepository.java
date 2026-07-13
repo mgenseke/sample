4185ec7e99b3bdb1539051d49624539aec228089
g1n <5nhxcgyw5j@privaterelay.appleid.com>
2026-07-13 13:18:44 +0200

    Add files via upload

---
+++ photos-clone/src/main/java/com/example/mng/photos_clone/PhotozRepository.java
@@ -0,0 +1,7 @@
+package com.example.mng.photos_clone;
+
+import com.example. mng.photos_clone.Photo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PhotozRepository extends CrudRepository<Photo, Integer> {
+}
